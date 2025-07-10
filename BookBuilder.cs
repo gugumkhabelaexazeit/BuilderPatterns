1d3b9e3c5b3a765dacd0c4ab2a9288de082e9505
gugu <Exaze Trsa25@EXZ_MP2HMR98>
2025-07-10 19:27:32 +0200

    Add project files.

---
+++ BookBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPatterns
+{
+    public class BookBuilder
+    {
+
+        private Book _book = new Book();
+
+        public BookBuilder setTitle(string title) // the method to set this fields 
+        {
+            _book.Title = title;
+            return this;  // method return self
+        }
+
+        public BookBuilder setAuthor(string author) 
+        { 
+        _book.Author = author;
+            return this;
+        }
+
+        public BookBuilder setpages(int page)
+        {
+            _book.Pages = page;
+            return this;
+        }
+
+        public Book Builder()
+        {
+            return _book;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using BuilderPatterns;
+
+var book = new BookBuilder()
+.setTitle("clean Archictecture")
+.setAuthor("rober")
+.setpages(100)
+.Builder();
+
+Console.ReadKey();
